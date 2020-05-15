bfbb9710d6e362c73c1e450634a5f03525c69f10
James Luttringer <jamesluttringer18@gmail.com>
2020-05-15 12:10:45 -0400

    sprint

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.oldest = 0
+        self.values = []
     def append(self, item):
-        pass
-
+        if len(self.values)<self.capacity:
+            self.values.append(item)
+        else:
+            self.values[self.oldest] = item
+            if self.oldest < self.capacity-1:
+                self.oldest += 1
+            else:
+                self.oldest = 0
     def get(self):
-        pass
+        return self.values
